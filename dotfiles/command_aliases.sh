e2dc17e192814f07f0f8598da6df74f929b5de97
Paul Sexton <paul.sexton@rallyhealth.com>
2022-07-23 08:29:26 -0500

    dotfiles: add command_aliases

---
+++ dotfiles/command_aliases.sh
@@ -0,0 +1,12 @@
+# Source this file from .profile or wherever
+
+# git aliases
+alias gut="git"
+alias gl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
+alias gco="git checkout"
+alias gcm="git commit -m"
+alias gp="git push"
+alias gs="git status"
+
+alias vsc="code"
+alias jcurl='curl --header "Content-Type: application/json"'
